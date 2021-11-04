f00c075dc345e424c43fb1535de5139a3c51e33f
Chiamaka Ugoji <mychiamaka@gmail.com>
2021-11-04 17:37:25 +0100

    added react dependencies and counter body

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+      <h1>0</h1>
+
+        <div className="btn">
+          <button className="btn-minus">minus</button>
+          <button className="btn-add">add</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
